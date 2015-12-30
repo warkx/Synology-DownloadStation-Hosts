ce2197650b1fba6e1a5b9d82f13a34ace6bbe8b7
Sanil Piat <warkxia@gmail.com>
2015-12-30 22:09:40 +0100

    Creation

---
+++ UpleaCom/UpleaComV1.5.1.php
@@ -0,0 +1,346 @@
+﻿<?php
+
+/*Auteur : warkx
+  Version originale Developpé le : 16/04/2015
+  Version : 1.5.1
+  Développé le : 10/05/2015
+  Description : Support des comptes premium et  gratuit
+*/ 
+
+/*Declaration des constantes. WX est un préfixe pour eviter d'avoir un conflit
+avec les constantes par défaut */
+
+define('WX_LINK', 'link');
+define('WX_QUERYAGAIN', 1);
+define('WX_NOTQUERYAGAIN', 0);
+
+class SynoFileHosting
+{
+    private $Url;
+    private $ORIGINAL_URL;
+    private $HostInfo;
+    private $Username;
+    private $Password;
+    private $CookieValue;
+    private $FILEID;
+    
+    private $COOKIE_FILE = '/tmp/uplea.cookie';
+    
+    private $GET_LINK_API = 'http://api.uplea.com/api/get-link';
+    private $LOGIN_URL = 'http://uplea.com/?lang=en';
+    private $ACCOUNT_TYPE_URL = 'http://uplea.com/account';
+    
+    private $FILEONLINE_REGEX = '`result":{.*?status":"(OK)".*?}`i';
+    private $FILENAME_REGEX = '`"filename":"(.*?)"`i';
+    private $FILEID_REGEX = '`"code":"(.*?)"`i'; 
+    private $FILEURL_REGEX = '`"(https?:\/\/[a-zA-Z0-9]+\.uplea\.com\/[^"]+)`i';
+    private $DOWNLOAD_WAIT_REGEX = '/jCountdown\({\s*timeText:(\d+)/i';
+    private $FREE_WAIT_REGEX = '`ulCounter\({\'timer\':(\d+)`i';
+    private $ACCOUNT_TYPE_REGEX = '`Uplea\s*free\s*member`i';
+    private $PREMIUM_REQUIRED_REGEX = '`You need to have a Premium subscription to download this file`i';
+    
+    private $GET_LINK_API_TAB = array(WX_LINK => '');
+    
+    public function __construct($Url, $Username, $Password, $HostInfo)
+    { 
+        $this->Url = $Url;
+        $this->ORIGINAL_URL = $Url;
+        $this->Username = $Username;
+        $this->Password = $Password;
+        $this->HostInfo = $HostInfo;
+    }
+
+    public function GetDownloadInfo() 
+    { 
+        $ret = false;
+        $page = false;
+        $this->GET_LINK_API_TAB[WX_LINK] = $this->Url;
+        $page = $this->GetInfos($this->GET_LINK_API_TAB, $this->GET_LINK_API);
+        
+        if($page != false)
+        {
+            preg_match($this->FILEONLINE_REGEX, $page, $fileonlinematch);
+            if(empty($fileonlinematch[1]))
+            {
+                $ret[DOWNLOAD_ERROR] = ERR_FILE_NO_EXIST;
+
+            }else
+            {
+                preg_match($this->FILEID_REGEX,$page,$fileidmatch);
+                if(empty($fileidmatch[1]))
+                {
+                    $ret[DOWNLOAD_ERROR] = ERR_UPATE_FAIL;                    
+                }else
+                {
+                    $this->FILEID = $fileidmatch[1];
+                    $this->Url = $this->MakeUrl();
+                    
+                    $VerifyRet = $this->Verify(false);
+                    if($VerifyRet == USER_IS_PREMIUM)
+                    {
+                        $ret = $this->DownloadPremium();
+                    }else
+                    {
+                        $ret = $this->DownloadWaiting($VerifyRet);
+                    }
+                
+                    preg_match($this->FILENAME_REGEX,$page,$filenamematch);
+                    if(!empty($filenamematch[1]))
+                    {
+                        $ret[DOWNLOAD_FILENAME] = $filenamematch[1];
+                    }
+                }
+            }
+        }else
+        {
+            $ret[DOWNLOAD_ERROR] = ERR_UPATE_FAIL;
+        }
+        
+        $ret[INFO_NAME] = trim($this->HostInfo[INFO_NAME]);
+        
+        return $ret; 
+    }
+
+    //se connecte et renvoie le type du compte
+    public function Verify($ClearCookie)
+    {
+        $ret = LOGIN_FAIL;
+        $this->CookieValue = false;
+  
+        //si le nom d'utilisateur et le mot de passe sont entré on se connecte
+        //renvoie le cookie si la connexion est initialisé
+        if(!empty($this->Username) && !empty($this->Password)) 
+        {
+            $this->CookieValue = $this->Login($this->Username, $this->Password);
+        }
+        //Verifie le type de compte
+        if($this->CookieValue != false) 
+        {
+            $ret = $this->AccountType($this->Username, $this->Password);
+        }
+        if (($ClearCookie OR $ret == LOGIN_FAIL) && file_exists($this->COOKIE_FILE)) 
+        {
+            unlink($this->COOKIE_FILE);
+        }
+        return $ret;
+    }
+    
+    
+    //Telechargement en mode premium
+    private function DownloadPremium()
+    {
+        $ret = false;
+        $DownloadInfo = array();
+        $ret = $this->UrlFilePremium();
+        
+        if($ret != false)
+        {
+            $DownloadInfo[DOWNLOAD_URL] = $ret;
+        }else
+        {
+            $DownloadInfo = $this->DownloadWaiting(USER_IS_PREMIUM);
+        }
+        return $DownloadInfo;
+    }
+    
+    
+    private function DownloadWaiting($VerifyRet)
+    {
+        $page = false;
+        $DownloadInfo = false;
+        $VerifyWait = false; 
+        
+        $page = $this->DownloadPage($this->ORIGINAL_URL);
+        $page = $this->DownloadPage($this->Url);
+        
+        if($page !=false)
+        {
+            preg_match($this->PREMIUM_REQUIRED_REGEX,$page, $errormatch);
+            if(isset($errormatch[0]))
+            {
+               $DownloadInfo[DOWNLOAD_ERROR] = ERR_REQUIRED_PREMIUM; 
+            }else
+            {
+                if($VerifyRet != USER_IS_PREMIUM)
+                {
+                   $VerifyWait = $this->VerifyWaitDownload($page); 
+                }
+            
+                if($VerifyWait != false)
+                {
+                    $DownloadInfo[DOWNLOAD_COUNT] = $VerifyWait;
+                }
+                
+                preg_match($this->FILEURL_REGEX, $page, $realurlmatch);
+                if(!empty($realurlmatch[1]))
+                {
+                    $DownloadInfo[DOWNLOAD_URL] = $realurlmatch[1];
+                    if($VerifyWait != false)
+                    {
+                        $DownloadInfo[DOWNLOAD_ISQUERYAGAIN] = WX_NOTQUERYAGAIN;
+                    }
+                }else
+                {
+                    $DownloadInfo[DOWNLOAD_ISQUERYAGAIN] = WX_QUERYAGAIN;
+                }
+            }            
+        }else
+        {
+            $DownloadInfo[DOWNLOAD_ERROR] = ERR_FILE_NO_EXIST;
+        }
+        
+        return $DownloadInfo;
+    }
+    
+    
+    private function DownloadPage($strUrl)
+    {
+        $ret = false;
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); 
+		curl_setopt($curl, CURLOPT_URL, $strUrl);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_COOKIEJAR, $this->COOKIE_FILE);	
+		curl_setopt($curl, CURLOPT_COOKIEFILE, $this->COOKIE_FILE);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$ret = curl_exec($curl);
+        curl_close($curl);
+        return $ret;
+    }
+    
+    private function Login()
+    {
+        $ret = false;
+        $LoginInfo = false;
+		$PostData = array('login' => $this->Username,
+                          'password' =>$this->Password,
+                          'remember' =>1,
+                          'login-form' =>'');
+            
+		$queryUrl = $this->LOGIN_URL;
+		$PostData = http_build_query($PostData);
+        
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $PostData);
+		curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
+		curl_setopt($curl, CURLOPT_COOKIEJAR, $this->COOKIE_FILE);
+		curl_setopt($curl, CURLOPT_HEADER, TRUE);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($curl, CURLOPT_URL, $queryUrl);
+		$LoginInfo = curl_exec($curl);
+		curl_close($curl);
+		
+		if ($LoginInfo != false && file_exists($this->COOKIE_FILE)) 
+        {
+            $CookieData = file_get_contents($this->COOKIE_FILE);
+			if(stripos($CookieData, '#HttpOnly_uplea.com'))
+            {
+                $ret = true;
+			}else 
+            {
+                $ret = false;
+            }
+		}
+        
+		return $ret;
+    }
+    
+    //renvoie premium si le compte est premium sinon concidere qu'il est gratuit
+    private function AccountType()
+    {
+        $ret = false;
+        $curl = curl_init();
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
+		curl_setopt($curl, CURLOPT_COOKIEFILE, $this->COOKIE_FILE);
+		curl_setopt($curl, CURLOPT_HEADER, TRUE);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($curl, CURLOPT_URL, $this->ACCOUNT_TYPE_URL);
+		$page = curl_exec($curl);
+		curl_close($curl);
+    
+        preg_match($this->ACCOUNT_TYPE_REGEX,$page,$accouttypematch);
+        if(isset($accouttypematch[0]))
+        {
+            $ret = USER_IS_FREE;
+        }else
+        {
+            $ret = USER_IS_PREMIUM;
+        }
+        return $ret;
+    }
+    
+    private function VerifyWaitDownload($page)
+    {
+        $ret = false;
+        
+        preg_match($this->DOWNLOAD_WAIT_REGEX, $page, $waitingmatch);
+        if(!empty($waitingmatch[1]))
+        {
+            $ret = $waitingmatch[1] + 10;
+        }else
+        {
+            preg_match($this->FREE_WAIT_REGEX, $page, $waitingmatch);
+            if(!empty($waitingmatch[1]))
+            {
+                $ret = $waitingmatch[1] +3;
+            }
+            
+        }
+        return $ret;
+    }
+    
+    private function MakeUrl()
+    {
+        $ret = 'http://uplea.com/step/'.$this->FILEID.'/3?lang=en';
+        return $ret;
+    }
+    
+    private function UrlFilePremium()
+    {
+        $ret = false;
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
+		curl_setopt($curl, CURLOPT_URL, $this->Url);
+		curl_setopt($curl, CURLOPT_COOKIEFILE, $this->COOKIE_FILE);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+		curl_setopt($curl, CURLOPT_HEADER, true);
+
+		$header = curl_exec($curl);
+		$info = curl_getinfo($curl);
+        curl_close($curl);
+    
+		$error_code = $info['http_code'];
+		
+		if ($error_code == 301 || $error_code == 302) 
+        { 
+            $url = $info['redirect_url'];
+            $ret = $url;
+		}
+		return $ret;
+    }
+    
+    
+    //recupere les informations des urls
+    private function GetInfos($tab, $linkAPI)
+    {
+        $ret = false;
+        $data = $tab;
+        $data = http_build_query($data);
+        $curl = curl_init();
+        
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); 
+        curl_setopt($curl, CURLOPT_URL, $linkAPI);
+        curl_setopt($curl, CURLOPT_POST, TRUE);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $ret = curl_exec($curl);
+        curl_close($curl);
+        
+        return $ret;
+    }
+}
+?>
---
+++ UptoboxCom/UptoboxComV1.0.3.php
@@ -0,0 +1,356 @@
+﻿<?php
+
+/*Auteur : warkx
+  Version : 1.0.3
+  Développé le : 06/08/2015
+  Description : Support du compte gratuit et premium*/
+  
+  
+class SynoFileHosting
+{
+    private $Url;
+    private $Username;
+    private $Password;
+    private $HostInfo;
+    private $CookieValue;
+  
+    private $COOKIE_FILE = '/tmp/uptobox.cookie';
+    private $LOGIN_URL = 'https://login.uptobox.com/log';
+    private $ACCOUNT_TYPE_URL = 'https://uptobox.com/?op=my_account';
+  
+    private $FILE_NAME_REGEX = '/name="fname"\s*value="(.*)"/i';
+    private $RAND_REGEX = '/name="rand"\s*value="(.*)"/i';
+    private $ID_REGEX = '/name="id"\s*value="(.*)"/i';
+    private $FILE_SIZE_REAL_REGEX = '/name="file_size_real"\s*value="(.*)"/i';
+    private $FILE_OFFLINE_REGEX = '/The file was deleted|Page not found/i';
+    private $DOWNLOAD_WAIT_REGEX = '/You have to wait ((\d+) minute)*/i';
+    private $FILE_URL_REGEX = '`<!--DOWNLOAD BUTTON-->.*?href="(.*?)">`si';
+    private $ACCOUNT_TYPE_REGEX = '/Premium\s*member/i';
+    private $ERROR_404_URL_REGEX = '/uptobox.com\/404.html/i';
+  
+    private $STRING_COUNT = 'count';
+    private $STRING_FNAME = 'fname';
+    private $QUERYAGAIN = 1;
+    private $WAITING_TIME_DEFAULT = 300;
+    
+    private $TAB_REQUEST = array('op' => 'download2',
+                                'id' => '',
+                                'fname' => '',
+                                'rand' => '',
+                                'refer' => '',
+                                'file_size_real'=>'',
+                                'method_free' =>'',
+                                'method_premium' => '',
+                                'down_direct' => '1');
+  
+    public function __construct($Url, $Username, $Password, $HostInfo) 
+    {
+		$this->Url = $Url;
+		$this->Username = $Username;
+		$this->Password = $Password;
+		$this->HostInfo = $HostInfo;
+	}
+  
+    //se connecte et renvoie le type du compte
+    public function Verify($ClearCookie)
+    {
+        $ret = LOGIN_FAIL;
+        $this->CookieValue = false;
+  
+        //si le nom d'utilisateur et le mot de passe sont entré on se connecte
+        //renvoie le cookie si la connexion est initialisé
+        if(!empty($this->Username) && !empty($this->Password)) 
+        {
+            $this->CookieValue = $this->Login($this->Username, $this->Password);
+        }
+        //Verifie le type de compte
+        if($this->CookieValue != false) 
+        {
+            $ret = $this->AccountType($this->Username, $this->Password);
+        }
+        if ($ClearCookie && file_exists($this->COOKIE_FILE)) 
+        {
+            unlink($this->COOKIE_FILE);
+        }
+        return $ret;
+    }
+    
+    //Lance le telechargement en fonction du type de compte
+    public function GetDownloadInfo()
+    {
+        $ret = false;
+        $VerifyRet = $this->Verify(false);
+    
+        if(USER_IS_PREMIUM == $VerifyRet)
+        {
+            $ret = $this->DownloadPremium();
+      
+        }else if(USER_IS_FREE == $VerifyRet)
+        {
+            $ret = $this->DownloadWaiting(true);
+        }else
+        {
+            $ret = $this->DownloadWaiting(false);
+        }
+    
+        if($ret != false)
+        {
+            $ret[INFO_NAME] = trim($this->HostInfo[INFO_NAME]);
+        }
+    
+        return $ret;
+    }
+  
+    //Telechargement en mode premium
+    private function DownloadPremium()
+    {
+        $ret = false;
+        $DownloadInfo = array();
+        $ret = $this->UrlFilePremium();
+        
+        if($ret == false)
+        {
+            $DownloadInfo[DOWNLOAD_ERROR] = ERR_FILE_NO_EXIST;
+        }else
+        {
+            $DownloadInfo[DOWNLOAD_URL] = $ret;
+            $DownloadInfo[DOWNLOAD_COOKIE] = $this->COOKIE_FILE;
+        }
+        return $DownloadInfo;
+    }
+    
+    //telechargement en mode gratuit ou sans compte
+    private function DownloadWaiting($LoadCookie)
+    {
+        $DowloadInfo = false;
+        $page = $this->DownloadParsePage($LoadCookie);
+        
+        if($page != false)
+        {
+            //Termine la fonction si le fichier est offline
+            preg_match($this->FILE_OFFLINE_REGEX,$page,$errormatch);
+            if(isset($errormatch[0]))
+            {
+                $DownloadInfo[DOWNLOAD_ERROR] = ERR_FILE_NO_EXIST;
+            }else
+            {
+                //verifie s'il faut attendre et si c'est le cas, renvoie le temps d'attente
+                $result = $this->VerifyWaitDownload($page);
+                if($result != false)
+                {
+                    $DownloadInfo[DOWNLOAD_COUNT] = $result[$this->STRING_COUNT];
+                    $DownloadInfo[DOWNLOAD_ISQUERYAGAIN] = $this->QUERYAGAIN;
+                }else
+                {
+                    //genere la requete pour cliquer sur "Generer le lien" et recupere le nom du fichier
+                    $this->GenerateRequest($page);
+                    $DownloadInfo[DOWNLOAD_FILENAME] = $this->TAB_REQUEST[$this->STRING_FNAME];
+                    
+                    //clique sur le bouton "Generer le lien" et recupere la vrai URL
+                    $page = $this->UrlFileFree($LoadCookie);
+                    preg_match($this->FILE_URL_REGEX,$page,$urlmatch);
+                    if(!empty($urlmatch[1]))
+                    {
+                        $DownloadInfo[DOWNLOAD_URL] = $urlmatch[1];
+                    }else
+                    {
+                        $DownloadInfo[DOWNLOAD_COUNT] = $this->WAITING_TIME_DEFAULT;
+                        $DownloadInfo[DOWNLOAD_ISQUERYAGAIN] = $this->QUERYAGAIN;
+                    }
+                }
+            }
+            if($LoadCookie == true)
+            {
+                $DownloadInfo[DOWNLOAD_COOKIE] = $this->COOKIE_FILE;
+            }
+        }
+        return $DownloadInfo;
+    }
+  
+    private function GenerateRequest($page)
+    {
+        preg_match($this->FILE_NAME_REGEX, $page, $filenamematch);
+        if(!empty($filenamematch[1]))
+        {
+            $this->TAB_REQUEST[$this->STRING_FNAME] = $filenamematch[1];
+        }
+        preg_match($this->RAND_REGEX, $page, $randmatch);
+        if(!empty($randmatch[1]))
+        {
+            $this->TAB_REQUEST['rand'] =  $randmatch[1];
+        }
+        preg_match($this->ID_REGEX, $page, $idmatch);
+        if(!empty($idmatch[1]))
+        {
+            $this->TAB_REQUEST['id'] =  $idmatch[1];
+        }
+        preg_match($this->FILE_SIZE_REAL_REGEX, $page, $filesizerealmatch);
+        if(!empty($filesizerealmatch[1]))
+        {
+            $this->TAB_REQUEST['file_size_real'] = $filesizerealmatch[1];
+        }
+    }
+    
+    //Renvoie le temps d'attente indiqué sur la page, ou false s'il n'y en a pas
+    private function VerifyWaitDownload($page)
+    {
+        $ret = false;
+    
+        preg_match($this->DOWNLOAD_WAIT_REGEX, $page, $waitingmatch);
+        if(!empty($waitingmatch[0]))
+        {
+            if(!empty($waitingmatch[2]))
+            {
+                $waitingtime = ($waitingmatch[2] *60) + 70;
+            }else
+            {
+                $waitingtime = 70;
+            }
+            $ret[$this->STRING_COUNT] = $waitingtime;
+        }
+        return $ret;
+    }
+  
+    //authentifie l'utilisateur sur le site
+    private function Login()
+    {
+        $ret = LOGIN_FAIL;
+		$PostData = array('login'=>$this->Username,
+                        'password'=>$this->Password,
+                        'op'=>'login');
+            
+		$queryUrl = $this->LOGIN_URL;
+		$PostData = http_build_query($PostData);
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $PostData);
+		curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
+		curl_setopt($curl, CURLOPT_COOKIEJAR, $this->COOKIE_FILE);
+		curl_setopt($curl, CURLOPT_HEADER, TRUE);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($curl, CURLOPT_URL, $queryUrl);
+		$LoginInfo = curl_exec($curl);
+		curl_close($curl);
+		
+		if ($LoginInfo != false && file_exists($this->COOKIE_FILE)) 
+        {
+			$cookieData = file_get_contents ($this->COOKIE_FILE);
+			if(strpos($cookieData,'xfss') == true) 
+            {
+				$ret = true;
+			}else 
+            {
+                $ret = false;
+            }
+		}
+		return $ret;
+    }
+  
+    //renvoie premium si le compte est premium sinon concidere qu'il est gratuit
+    private function AccountType()
+    {
+        $ret = false;
+        $curl = curl_init();
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
+		curl_setopt($curl, CURLOPT_COOKIEFILE, $this->COOKIE_FILE);
+		curl_setopt($curl, CURLOPT_HEADER, TRUE);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($curl, CURLOPT_URL, $this->ACCOUNT_TYPE_URL);
+		$page = curl_exec($curl);
+		curl_close($curl);
+    
+        preg_match($this->ACCOUNT_TYPE_REGEX,$page,$accouttypematch);
+        if(isset($accouttypematch[0]))
+        {
+            $ret = USER_IS_PREMIUM;
+        }else
+        {
+            $ret = USER_IS_FREE;
+        }
+        return $ret;
+    }
+    
+    //affiche la page en mode gratuit
+    private function DownloadParsePage($LoadCookie)
+    {
+        $ret = false;
+        $curl = curl_init(); 
+    
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); 
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_USERAGENT,DOWNLOAD_STATION_USER_AGENT);
+        if($LoadCookie == true)
+        {
+            curl_setopt($curl, CURLOPT_COOKIEFILE, $this->COOKIE_FILE);
+        }
+        curl_setopt($curl, CURLOPT_HEADER, TRUE);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE); 
+        curl_setopt($curl, CURLOPT_URL, $this->Url); 
+    
+        $ret = curl_exec($curl); 
+        $info = curl_getinfo($curl);
+        curl_close($curl);
+    
+        $this->Url = $info['url'];
+        return $ret; 
+    }
+  
+    //renvoie la vrai URL du fichier en mode gratuit
+    private function UrlFileFree($LoadCookie)
+    {
+        $ret = false;
+        $data = $this->TAB_REQUEST;
+        $data = http_build_query($data);
+        $curl = curl_init();
+    
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); 
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_USERAGENT,DOWNLOAD_STATION_USER_AGENT);
+        if($LoadCookie == true)
+        {
+            curl_setopt($curl, CURLOPT_COOKIEFILE, $this->COOKIE_FILE);
+        }
+        curl_setopt($curl, CURLOPT_POST, TRUE);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        curl_setopt($curl, CURLOPT_URL, $this->Url);
+    
+        $header = curl_exec($curl);
+        curl_close($curl);
+    
+        $ret = $header;
+        return $ret;
+    }
+  
+    //renvoie la vrai url du fichier en mode premium. Ou false si elle n'est pa affiché
+    private function UrlFilePremium()
+    {
+        $ret = false;
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_USERAGENT, DOWNLOAD_STATION_USER_AGENT);
+		curl_setopt($curl, CURLOPT_URL, $this->Url);
+		curl_setopt($curl, CURLOPT_COOKIEFILE, $this->COOKIE_FILE);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+		curl_setopt($curl, CURLOPT_HEADER, true);
+
+		$header = curl_exec($curl);
+		$info = curl_getinfo($curl);
+        curl_close($curl);
+    
+		$error_code = $info['http_code'];
+		
+		if ($error_code == 301 || $error_code == 302) 
+        { 
+			$ret = $info['redirect_url'];
+		}
+        preg_match($this->ERROR_404_URL_REGEX, $ret, $finderror);
+        if(isset($finderror[0]))
+        {
+            $ret = false;
+        }
+		return $ret;
+    }
+}
+?>
